655fb22a23601585b9729851bbb33f710d4958e6
msreevani2109 <72864103+msreevani2109@users.noreply.github.com>
2024-08-11 13:26:01 +0530

    Update script.js with custom close button

--- script.js
+++ script.js
@@ -44,6 +44,9 @@ function navigate(form){
       alert("logged in succesfully" );
     form.action = "home.html";
 }
+document.getElementById("apt-custom-close").onclick = function(){
+  aptrinsic('track','apt-custom-close',{"clicked":"yes"});
+}
 function handleYes(){
   aptrinsic('track','track-yes',{"respose":"yes"});
 }
