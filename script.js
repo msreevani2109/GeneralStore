897265375629dda33b0949f7128c5192ecfabaac
msreevani2109 <72864103+msreevani2109@users.noreply.github.com>
2024-08-20 19:38:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ function CallVisitorIdentify(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-EINSODADBSWN-2");
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-EINSODADBSWN-2",{
+      "engagementChecksumFileUrl":"Engagement_checksum_sreevani webapp_Production.json"
+  });
 //Gainsight PX Tag - end
 
 function getLastName(){
