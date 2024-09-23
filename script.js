8b154deecafb1cdcc61bcac06db9519eeba536d9
msreevani2109 <72864103+msreevani2109@users.noreply.github.com>
2024-09-23 17:00:49 +0530

    Update script.js by reverting key changes

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function CallVisitorIdentify(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-6QPYIVOVTFT3-2",{
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-EINSODADBSWN-2",{
       //"engagementChecksumFileUrl":"newchecksum.json"
   });
 //Gainsight PX Tag - end
