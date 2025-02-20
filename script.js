3e0b74bde5a27944a3f0d45e617f2375a1b060a4
gs-msreevani <msreevani@gainsight.com>
2025-02-20 16:11:53 +0530

    adding checksum file

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function CallVisitorIdentify(){
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
   })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-EINSODADBSWN-2",{
-     // "engagementChecksumFileUrl":"newchecksum.json"
+     "engagementChecksumFileUrl":"newchecksum.json"
   });
 //Gainsight PX Tag - end
 
@@ -101,3 +101,4 @@ function setGlobalContext(context){
 function handleQuery(){
     aptrinsic('track',"Connect Project selected");
 }
+
