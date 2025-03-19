2b52214bc72896ab0dc0385dc100eef111f539f2
gs-msreevani <msreevani@gainsight.com>
2025-03-20 04:32:19 +0530

    changing product key

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-EINSODADBSWN-2",{
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-OM2DTW6NF8A7-2",{
      //"engagementChecksumFileUrl":"newchecksum.json",
      badgesUseFixedPosition: true
   });
