7c15d4f8763700af0cd31d05a66e2a82fa163d1a
gs-msreevani <msreevani@gainsight.com>
2025-12-17 19:36:56 +0530

    adding new congig

--- script.js
+++ script.js
@@ -32,6 +32,7 @@
   {
     engagementChecksumFileUrl:
       "Engagement_checksum_sreevani webapp_Production (23).json",
+    skipChecksumForEngagementIds: ["c5aaadd3-1d4f-49f9-a141-377489a885c8"],
     //badgesUseFixedPosition: true
     //htmlSanitization: true,
     //htmlSanitizationAllowedDomains: ["//player.vimeo.com/video/1074485388"],
